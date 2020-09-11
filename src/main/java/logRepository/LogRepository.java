0e871e94bbf62084e7a237531c4c54ef13cbb55f
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-11 12:32:05 +0530

    Get NewRowCount into Main.class

--- src/main/java/logRepository/LogRepository.java
+++
@@ -1,9 +0,0 @@
-package logRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface LogRepository {
-
-    public ArrayList<String>actionPerform(ArrayList<String> arrayList);
-}
