c56c272fff1b46e9d61f22d878bbcb8f3488a127
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-12 05:32:42 +0530

    Test

--- src/main/java/logRepository/LogRepository.java
+++ src/main/java/logRepository/LogRepository.java
@@ -16,6 +16,7 @@ public class LogRepository {
     boolean isReadFromBegin=false;
     String lastTimeStamp;
     ArrayList<String> errorTimeList=new ArrayList<>();
+    int count =0;
 
     public ArrayList<String> getErrorTimeList(String path, String lastTimeStamp) throws IOException {
         this.lastTimeStamp=lastTimeStamp;
@@ -24,16 +25,22 @@ public class LogRepository {
         }
         try (Stream<String> lines = Files.lines(Paths.get(path), Charset.defaultCharset())) {
             lines.forEachOrdered(line -> process(line));
+
         }
         return errorTimeList;
     }
 
     private void process(String line) {
-        if(isReadFromBegin && line.indexOf("ERROR")!=-1){
-            errorTimeList.add(line.split(" ERROR")[0]);
-        }
-        else if(line.contains(lastTimeStamp)){
-            isReadFromBegin=true;
+//        if(isReadFromBegin && line.indexOf("ERROR")!=-1){
+//            errorTimeList.add(line.split(" ERROR")[0]);
+//        }
+//        else if(line.contains(lastTimeStamp)){
+//            isReadFromBegin=true;
+//        }
+        count++;
+        if(line.indexOf("2020-09-13T02:37:57Z")!=-1){
+            System.out.println(line);
+            System.out.println(count);
         }
     }
 }
