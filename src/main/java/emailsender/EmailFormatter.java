57e325a0f6e87253bc1a901add5fc31e3405c216
edpchinthana <edpchinthana@outlook.com>
2020-09-13 21:01:51 +0530

    email formatter initial

--- src/main/java/emailsender/EmailFormatter.java
+++ src/main/java/emailsender/EmailFormatter.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class EmailFormatter {
     String formatEmailBody(List<String> errorList){
         String body = "";
-
+        //todo : implement formatter after adding LogLine model
         return body;
     }
 }
