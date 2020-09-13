cb0b7e432a4afd93a2d60bf9fb5c976b3775efbe
edpchinthana <edpchinthana@outlook.com>
2020-09-13 20:23:05 +0530

    mysql email repository

--- src/main/java/emailRepository/EmailRepository.java
+++ src/main/java/emailRepository/EmailRepository.java
@@ -1,4 +1,14 @@
 package emailRepository;
 
+import java.util.List;
+import models.Email;
+
 public interface EmailRepository {
+    List<Email> getEmails();
+
+    void addEmail(Email email);
+
+    void deleteEmail(int id);
+
+    void updateEmail(Email email);
 }
