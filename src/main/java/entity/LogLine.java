d83c3b1dc3572b6f9d704d46f10d08eac7b0d029
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-13 15:53:37 +0530

    encapsulated logLine class

--- src/main/java/entity/LogLine.java
+++ src/main/java/entity/LogLine.java
@@ -6,4 +6,22 @@ package entity;
 
 
 public class LogLine {
+    private String timeStamp;
+    private String message;
+
+    public LogLine() {
+    }
+
+    public LogLine(String timeStamp, String message) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
