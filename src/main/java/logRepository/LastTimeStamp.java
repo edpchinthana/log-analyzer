c4e02bfbbb0d2e446e75df40b586efc5e5824d18
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-15 04:53:19 +0530

    create getLastTimeStamp function

---
+++ src/main/java/logRepository/LastTimeStamp.java
@@ -0,0 +1,24 @@
+package logRepository;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * Created by Padma Gnanapiya (SE/2017/014)
+ */
+
+
+public class LastTimeStamp {
+    public String getLastTimeStamp(String logPath) throws IOException {
+        LineIterator lineIterator = FileUtils.lineIterator(new File(logPath),"UTF-8");
+        String lastLine="";
+        while (lineIterator.hasNext()){
+            lastLine=  lineIterator.nextLine();
+        }
+        return lastLine;
+    }
+
+}
