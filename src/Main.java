9a448e2641b1b2ed6affe2149eb688c58ec9e9cb
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-14 11:41:21 +0530

    create GetLastTimeStamp from the text file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
+import configurationsRepository.GetLastTimeStamp;
+
 public class Main {
+    public static void main(String[] args) {
+        GetLastTimeStamp getLastTimeStamp=new GetLastTimeStamp();
+        getLastTimeStamp.actionPerform("H:\\A_Padma_Codes\\Code_Java\\log-analyzer\\src\\test.log");
+    }
 }
--- src/configurationsRepository/GetLastTimeStamp.java
+++ src/configurationsRepository/GetLastTimeStamp.java
@@ -1,13 +1,42 @@
 package configurationsRepository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by Padma Gnanapiya (SE/2017/014)
  */
 
 
 public class GetLastTimeStamp implements ConfigurationsRepository{
+    String timeStamp=null;
     @Override
-    public String actionPerform(String path) {
-        return null;
+    public String actionPerform(String  logLath) {
+        Path path = Paths.get(recordPath);
+        try {
+
+            List contents = Files.readAllLines(path);
+
+            //Read from the stream
+            for (Object content : contents) {//for each line of content in contents
+                if(content.toString().startsWith(logLath)) {
+                    System.out.println(content);// print the line
+                }
+
+
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();//handle exception here
+        }
+        return recordPath;
+
     }
 }
