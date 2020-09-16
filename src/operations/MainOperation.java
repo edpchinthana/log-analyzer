e2e9f4a3fb68478aa61e27ae58e85a46b6ebce2c
hansikaherath <kavihanhe111@gmail.com>
2020-09-16 08:38:15 +0530

    update

--- src/operations/MainOperation.java
+++ src/operations/MainOperation.java
@@ -1,14 +1,22 @@
 package operations;
 
+import operations.logAnalyze.inputOperation;
 import operations.logAnalyze.printMenuOperation;
 
 public class MainOperation {
-    public static void main(String args[]) {
-
+    public static void main(String []args) {
+        //log Analyzer part
         //call to print menu operation
         printMenuOperation menu=new printMenuOperation();
         menu.printMenu();
 
+        //switch in input
+        inputOperation input=new inputOperation();
+        input.checkInput();
+
+        //Email manage part
+
+        //call to exit
 
     }
 }
--- src/operations/logAnalyze/inputOperation.java
+++ src/operations/logAnalyze/inputOperation.java
@@ -1,49 +1,49 @@
 package operations.logAnalyze;
 
-import operations.EmailManage.addEmailOperation;
-import operations.EmailManage.deletEmailOperation;
-import operations.EmailManage.getEmailOperation;
 import operations.exit.userExit;
-import input.#;
-
-import java.awt.*;
-import java.util.List;#;
+#;#;
 
 public class inputOperation {
 
-    public static void checkInput(){
+    public String checkInput(){
 
         //call to get user input
         CommandLineInput item=new CommandLineInput();
         int fact= item.readMenuItem();
 
+
         switch (fact){
+
             case 1:
-                askpathoutput path= new askpathoutput();    //get file path and pass
+                //get file path and pass
+                askpathoutput path= new askpathoutput();
                 path.methodname();
                 //get list
                 getlistoferror errormsg=new gerlistoferror();
                 errormsg.method-name();
                 //pass error list
-
                 break;
+
             case 2:
+
                 //get summary details  from log repo error,info,warn
              //   List<Integer> numbers=numberRepository.read();
-
                 //displaysummary
-
                 calldisplaysummaryclass summary=new calldisplaysummaryclass();
-
                 summary.methodisplaysum( error,info, warns);
                 break;
+
             case 3:
+
                 //call email menu from output package
                 askemailmenu emailmenu= new askmailmenu();
                 emailmenu.methodmeilmenu();
                 //get user input as a- b- c-
                 emailuserinputclass chose=new emailuserinputclass();
                  String s=chose.methodn();
+                 return s;
+
+                 /*
                 //choose one option
                 if(s.equals("a")){
                     addEmailOperation addemail=new addEmailOperation();
@@ -54,8 +54,9 @@ public class inputOperation {
                 }else{
                     getEmailOperation getEmail=new getEmailOperation();
                     getEmail.get();
-                }
+                }*/
                 break;
+
             default:
                 //save current timestamp in the text file-overwrite
                 //exit from the application call user exit
@@ -63,5 +64,6 @@ public class inputOperation {
                 obj.appExit();
                 break;
         }
+        return null;
     }
 }
