335b9054b12bf10fd783a62593660ff311023195
hansikaherath <kavihanhe111@gmail.com>
2020-09-16 15:26:45 +0530

    add operationRepository

---
+++ src/main/java/operations/email/operationRepository.java
@@ -0,0 +1,23 @@
+package operations.email;
+
+public class operationRepository {
+    public void divideSections(){
+
+        //get s from inputOperation class checkinut case 3
+        CommandLineInput item=new CommandLineInput();
+        int n= item.readMenuItem();
+
+        //choose one option
+
+        if(n==1){
+            ViewEmailOperation getEmail=new ViewEmailOperation();
+            getEmail.view();
+        }else if(n==2){
+            AddEmailOperation addemail=new AddEmailOperation();
+            addemail.add();
+        }else{
+            DeletEmailOperation deletemail=new DeletEmailOperation();
+            deletemail.delete();
+        }
+    }
+}
