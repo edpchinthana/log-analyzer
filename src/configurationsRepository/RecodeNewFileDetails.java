63a7072dd8a421f59b2e7604fd10cfe4628014fc
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-18 14:53:32 +0530

    Create RecodeNewFileDetails

--- src/configurationsRepository/RecodeNewFileDetails.java
+++ src/configurationsRepository/RecodeNewFileDetails.java
@@ -1,13 +1,22 @@
 package configurationsRepository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**
  * Created by Padma Gnanapiya (SE/2017/014)
  */
 
 
-public class RecodeNewFileDetails implements ConfigurationsRepository{
-    @Override
-    public String actionPerform(String path) {
-        return null;
+public class RecodeNewFileDetails{
+//    @Override
+    public void actionPerform(String logPath, String recordPath) {
+        try {
+            Files.write(Paths.get(recordPath), (logPath + "\n").getBytes(),StandardOpenOption.CREATE,StandardOpenOption.APPEND);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
