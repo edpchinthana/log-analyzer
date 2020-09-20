2d67a044250d43e72274bc9285b07174744e4e3f
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-20 19:56:28 +0530

    Create ExportConfiguration

---
+++ src/main/java/configurations/impl/ExportConfiguration.java
@@ -0,0 +1,12 @@
+package configurations.impl;
+
+import models.ConfigurationModel;
+
+/**
+ * Created by Padma Gnanapiya (SE/2017/014)
+ */
+
+
+public interface ExportConfiguration {
+    void perform(ConfigurationModel var1, String var2);
+}
