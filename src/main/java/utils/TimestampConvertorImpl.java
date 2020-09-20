fed2512806d160583bcf71f502aa24c35dcdabe3
hansikaherath <kavihanhe111@gmail.com>
2020-09-20 20:10:23 +0530

    TimestampConvertorImpl code

--- src/main/java/utils/TimestampConvertorImpl.java
+++ src/main/java/utils/TimestampConvertorImpl.java
@@ -1,4 +1,22 @@
 package utils;
 
-public class TimestampConvertorImpl {
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimestampConvertorImpl implements TimestampConvertor{
+    final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+    public Timestamp stringToTimestamp(String dateStr) throws ParseException {
+        Date date = dateFormat.parse(dateStr);
+        return new Timestamp(date.getTime());
+    }
+
+    @Override
+    public Boolean compareTo(Timestamp timestamp1, Timestamp timestamp2) {
+        return timestamp1.after(timestamp2);
+    }
+
+
 }
