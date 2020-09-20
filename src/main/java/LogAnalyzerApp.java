c9ba1fbac86b5aa7a180d3dcb849ac17ed18d549
edpchinthana <edpchinthana@outlook.com>
2020-09-20 21:18:39 +0530

    runApp

--- src/main/java/LogAnalyzerApp.java
+++ src/main/java/LogAnalyzerApp.java
@@ -1,5 +1,12 @@
-public class LogAnalyzerApp {
-    public void runApp(){
+import configurations.ConfigurationRepository;
+import emailrepository.EmailRepository;
+import input.Input;
+import output.Output;
 
+public class LogAnalyzerApp {
+    public void runApp(Input input, Output output, ConfigurationRepository configurationRepository, EmailRepository emailRepository){
+        while(true){
+
+        }
     }
 }
