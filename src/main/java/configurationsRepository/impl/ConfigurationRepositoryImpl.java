baf8986e081b4d96bc41f51ee372190248e6b5b1
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-20 19:22:08 +0530

    ConfigurationRepositoryImpl class

---
+++ src/main/java/configurationsRepository/impl/ConfigurationRepositoryImpl.java
@@ -0,0 +1,12 @@
+package configurationsRepository.impl;
+
+import configurationsRepository.ConfigurationRepository;
+
+/**
+ * Created by Padma Gnanapiya (SE/2017/014)
+ */
+
+
+public class ConfigurationRepositoryImpl implements ConfigurationRepository {
+
+}
