d3f364c0b9d9ea03194c2a31b5591a30a58c3383
hansikaherath <kavihanhe111@gmail.com>
2020-09-20 20:04:44 +0530

    interface commit

--- src/main/java/utils/TimestampConvertor.java
+++ src/main/java/utils/TimestampConvertor.java
@@ -1,4 +1,9 @@
 package utils;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+
 public interface TimestampConvertor {
+    Timestamp stringToTimestamp(String dateStr) throws ParseException;
+    Boolean compareTo(Timestamp timestamp1, Timestamp timestamp2);
 }
