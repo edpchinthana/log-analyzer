dc0a77f12f3a999649332f12e7bc2106c76268ec
hansikaherath <kavihanhe111@gmail.com>
2020-09-20 13:05:44 +0530

    Emailfactory

--- src/main/java/operations/email/ManageEmialRepository.java
+++ src/main/java/operations/email/ManageEmialRepository.java
@@ -1,4 +1,21 @@
 package operations.email;
 
-public class ManageEmialRepository {
+import input.Input;
+import input.commandline.CommandLineInput;
+import operations.mainmenu.MainMenuOperation;
+import output.Output;
+import output.commandline.CommandlineUserOutput;
+
+public class ManageEmailRepository implements MainMenuOperation {
+    @Override
+    public void execute() {
+        Output output = new CommandlineUserOutput();
+        Input input = new CommandLineInput();
+        EmailMenuOperationFactory emailMenuOperationFactory = new EmailMenuOperationFactory();
+
+        output.displayEmailMenu();
+        EmailMenuOperation operation = emailMenuOperationFactory.getInstance( input.readMenuItem(4));
+        operation.execute();
+
+    }
 }
--- src/main/java/operations/email/ViewEmails.java
+++ src/main/java/operations/email/ViewEmails.java
@@ -1,4 +1,23 @@
 package operations.email;
 
-public class ViewEmails {
+import emailRepository.EmailRepository;
+import emailRepository.mysql.MysqlEmailRepository;
+import input.Input;
+import input.commandline.CommandLineInput;
+import models.Email;
+
+import java.util.List;
+
+public class ViewEmails implements EmailMenuOperation{
+
+    @Override
+    public void execute() {
+        EmailRepository emailRepository = new MysqlEmailRepository();
+
+        System.out.println("Email Addresses");
+        List<Email> emailList = emailRepository.getEmails();
+        for(Email email: emailList){
+            System.out.println(email.getName()+" "+email.getEmail_address());
+        }
+    }
 }
