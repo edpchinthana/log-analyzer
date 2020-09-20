dbd5e2f3117f17b8ba1f9c3c7d44c3cbd486fd13
PadmaGnanapriya <padmaisuru@gmail.com>
2020-09-20 19:45:27 +0530

    Create ImportConfiguration

---
+++ src/main/java/configurations/impl/ImportConfiguration.java
@@ -0,0 +1,12 @@
+package configurations.impl;
+
+import models.ConfigurationModel;
+
+/**
+ * Created by Padma Gnanapiya (SE/2017/014)
+ */
+
+
+public interface ImportConfiguration {
+    ConfigurationModel perform(String var1);
+}
